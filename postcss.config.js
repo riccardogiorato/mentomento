c05ba274c770f08ce57e128e34ee017fa634a552
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2020-11-05 21:04:57 +0100

    fix purgecss on production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: ["tailwindcss"],
+  plugins: [
+    "tailwindcss",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            content: ["./pages/**/*.js"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+  ],
 };
