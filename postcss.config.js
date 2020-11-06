0616390cbec7f60a93e0e4f82abb70544aa9ba9c
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2020-11-06 20:50:01 +0100

    add back defaultExtractor

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,21 @@ module.exports = {
       "@fullhuman/postcss-purgecss",
       process.env.NODE_ENV === "production"
         ? {
+            // the paths to all template files
             content: [
               "./pages/**/*.{js,jsx,ts,tsx}",
               "./components/**/*.{js,jsx,ts,tsx}",
             ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) => {
+              // Capture as liberally as possible, including things like `h-(screen-1.5)`
+              const broadMatches =
+                content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
+              // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+              const innerMatches =
+                content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
+              return broadMatches.concat(innerMatches);
+            },
           }
         : false,
     ],
